bd6d28f754526f52040dff89d82d05b9a61ea9a7
Rob Lewis <rob.y.lewis@gmail.com>
2020-06-23 15:15:23 +0200

    milestone

--- src/tests2.lean
+++ src/tests2.lean
@@ -33,7 +33,10 @@ do s ← command_comp.cmd <$> until_antiquot,
 #eval run_string components "abcd ef ♭ee♭ee" -/
 
 reserve notation `♭`
+reserve_notation `begin_mm_block`
 reserve notation `end_mm_block`
+reserve notation `as`
+reserve notation `image`
 
 section
 open widget
@@ -94,9 +97,17 @@ do pe ← parser.pexpr 10000,
    if tpe = `(string) then do  s ← eval_expr' string e, return $ command_comp.cmd s
    else return $ command_comp.antiquot e
 
-meta def parse_cmd_list : lean.parser $ list command_comp :=
-tk "end_mm_block" >> return [] <|>
-list.cons <$> parse_component <*> parse_cmd_list
+meta def parse_cmd_list_aux : lean.parser $ list command_comp :=
+-- do  pos ← cur_pos,
+--tk "end_mm_block" >> return (pos, []) <|>
+tk ";" >> return  [] <|>
+do c ← parse_component, cs ← parse_cmd_list_aux, return (c::cs)
+-- list.cons <$> (prod.mk pos <$> parse_component) <*> parse_cmd_list
+
+meta def parse_cmd_list : lean.parser $ bool × pos × list command_comp :=
+do pos ← cur_pos,
+   is_img ← option.is_some <$> (tk "as" >> tk "image")?,
+   prod.mk is_img <$> prod.mk pos <$> parse_cmd_list_aux
 
 /-
 meta def run_command_on (cmd : string → string) (e : expr) : tactic pexpr :=
@@ -110,18 +121,30 @@ meta def command_comp.translate : command_comp → tactic string
 | (command_comp.antiquot p) := let s := mathematica.form_of_expr  p in
 return $ "Activate[LeanForm[" ++ s ++ "]]"
 
-meta def execute_list (l : list command_comp) : tactic pexpr :=
+meta def execute_list (is_img : bool) (l : list command_comp) : tactic pexpr :=
 do l ← l.mmap command_comp.translate, --tactic.trace $ string.join l,
-   s ← mathematica.execute_and_eval $ string.join l,
+   let cmd := if is_img then "MakeDataUrlFromImage[" ++ string.join l ++ "]" else string.join l,
+   s ← mathematica.execute_and_eval cmd,
    mathematica.pexpr_of_mmexpr mathematica.trans_env.empty s
 
 -- return ``(())
 
+meta def string_of_pos_comp : bool × pos × list command_comp → tactic (bool × pos × string)
+| ⟨is_img, p, c⟩ := prod.mk is_img <$> prod.mk p <$> (execute_list is_img c >>= to_expr >>= eval_expr string)
+
 @[user_command] meta def parse_test (_ : parse (tk "begin_mm_block")) : lean.parser unit :=
-do ⟨ln, c⟩ ← cur_pos, l ← parse_cmd_list, e ← execute_list l, e ← to_expr e,--, parser.of_tactic (to_expr e >>= tactic.trace )
+do ⟨ln, c⟩ ← cur_pos,
+   l ← parse_cmd_list*,
+   tk "end_mm_block",
+   l ← l.mmap (λ e, string_of_pos_comp e),
+   l.mmap' $ λ ⟨is_img, ⟨ln, c⟩, e⟩,
+     save_widget ⟨ln, c - ("begin_mm_block".length - 1)⟩ $
+       if is_img then (url_component e) else _
+--   e ← ↑(l.mmap $ λ l', execute_list l' >>= to_expr),
+--    skip
 --   tactic.trace "e", tactic.trace e,
-   e ← eval_expr string e,
-   save_widget ⟨ln, c - ("begin_mm_block".length - 1)⟩ (url_component e) --    save_widget p mycomp
+--    e ← eval_expr string e,
+--    save_widget ⟨ln, c - ("begin_mm_block".length - 1)⟩ (url_component e) --    save_widget p mycomp
 .
 
 end
@@ -132,24 +155,21 @@ begin_mm_block
 
     4+4
 
-]"
+]";
+
+as image "Plot3D[x^2-y,{x,-3,3},{y,-3,3}]";
 
 end_mm_block
 
-#exit
 constant x : ℝ
 open real
 
 begin_mm_block
 
-"MakeDataUrlFromImage[
-
-    PlotOverX["((sin x)^2 - x)",{"x",-10,10}]
-
-]"
+as image
+"PlotOverX["((sin x)^2 - x)",{"x",-10,10}]";
 
 end_mm_block
-
 #exit
 
 

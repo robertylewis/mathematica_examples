c9e94909015f135f6885f6d4ddf890affc03cf4c
Rob Lewis <rob.y.lewis@gmail.com>
2020-07-23 16:48:29 +0200

    update version

--- src/tests2.lean
+++ src/tests2.lean
@@ -1,7 +1,7 @@
 import tactic.core
 import mathematica
 import data.complex.exponential
-
+import tactic.interactive_expr
 
 reserve notation `begin_mm_block`
 reserve notation `end_mm_block`
@@ -12,25 +12,29 @@ reserve notation `unfolding`
 section
 open widget
 
+
+#check component
+
 /-- This is a helper that should really be in core. You can't use component.stateless on
 tactic_state because you can't decide equality on them. -/
+
 meta def component.stateless' {π α} (view : π → list (html α)) : component π α :=
-component.mk
+component.with_should_update (λ _ _, tt) $ component.pure view /- component.mk
   α
   unit
   (λ _ _, ())
   (λ _ _ a, (⟨⟩, some a))
   (λ p _, view p )
   (λ _ _, ff) -- ⟵ this means that the component should not attempt to check that
-              -- the props are equal and just always update
+              -- the props are equal and just always update -/
 
-meta def url_component (src : string) : component tactic_state string :=
+meta def url_component (src : string) : component tactic_state empty :=
 component.stateless' $ λ ts,
   [h "h1" [] ["Mathematica output"],
     h "img" [attr.val "src" src] []
   ]
 
-meta def expr_widget (src : expr) : tactic (component tactic_state string) :=
+meta def expr_widget (src : expr) : tactic (component tactic_state empty) :=
 do s ← tactic.pp src,
   return $ component.stateless' $ λ ts,
     [h "h1" [] ["Mathematica output"],
@@ -99,13 +103,12 @@ do e ← execute_list to_unfold is_img c >>= to_expr,
 -- prod.mk is_img <$> prod.mk p <$> (execute_list is_img c >>= to_expr >>= eval_expr string)
 
 meta def make_widget (to_unfold : list name) (p : bool × pos × list command_comp) :
-  tactic $ pos × (widget.component tactic_state string) :=
+  tactic $ pos × (widget.component tactic_state empty) :=
 do (loc, data) ← string_of_pos_comp to_unfold p,
 match data with
 | sum.inl s := return $ ⟨loc, url_component s⟩
 | sum.inr e := prod.mk loc <$> expr_widget e
 end
-
 
 @[user_command] meta def parse_test (_ : parse (tk "begin_mm_block")) : lean.parser unit :=
 do --⟨ln, c⟩ ← cur_pos,
@@ -114,29 +117,45 @@ do --⟨ln, c⟩ ← cur_pos,
    tk "end_mm_block",
    l ← l.mmap (λ e, make_widget (to_unfold.get_or_else []) e),
    l.mmap' $ λ ⟨⟨ln, c⟩, w⟩,
-     save_widget ⟨ln, c - ("begin_mm_block".length - 1)⟩ w
+     trace "saving at " >> trace (⟨ln, c - ("begin_mm_block".length - 1)⟩ : pos) >>
+     save_widget ⟨ln, c - ("begin_mm_block".length - 1)⟩  w
+
+open widget
+ -- #find component tactic_state empty
+
+run_cmd save_widget ⟨125, 0⟩ $ widget.component.with_should_update (λ _ _, tt) $ component.pure $
+λ _,
+/- [h "ul" [] [
+     h "li" [] ["this is list item 1"],
+     h "li" [] ["this is list item 2"],
+     h "hr" [] [],
+     h "li" [] [
+          h "span" [] ["there is a button here"],
+          h "button" [on_click (λ _, 3)] ["click me!"]
+     ]
+]] -- -/[h "p" [attr.style [("color", "blue")]] ["hi"]]
+
+
 
 end
-
 
 constant x : ℝ
 open real
 
 noncomputable def f : ℝ → ℝ := λ x, sin x + cos x
 
-
 begin_mm_block  (unfolding f)
 "4+4";
 
-as image "Plot3D[x^2-y, {x,-3,3}, {y,-3,3}]";
+-- as image "Plot3D[x^2-y, {x,-3,3}, {y,-3,3}]";
 
-as image
-"PlotOverX["((sin x)^2 - x)", {"x",-10,10}]";
+-- as image
+-- "PlotOverX["((sin x)^2 - x)", {"x",-10,10}]";
 
-"Factor["(x^2-2*x+1)"]";
+-- "Factor["(x^2-2*x+1)"]";
 
-as image
-"Plot["f"[y], {y,-2,2}]";
+-- as image
+-- "Plot["f"[y], {y,-2,2}]";
 
 end_mm_block
 
